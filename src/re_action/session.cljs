ef7bdc10deb10bc462fe4ce5c9a68ee4538a2850
stanimirovic <markostanimirovic95@gmail.com>
2019-07-29 21:23:59 +0200

    Created session namespace with core functions.

---
+++ src/re_action/session.cljs
@@ -0,0 +1,12 @@
+(ns re-action.session
+  (:require [re-streamer.core :as re-streamer :refer [emit]]))
+
+(defonce ^:private store (re-streamer/behavior-stream))
+
+(defn put! [key value]
+  (emit store (assoc @(:state store) key value)))
+
+(defn get [key]
+  (-> store
+      (re-streamer/map key)
+      (re-streamer/distinct =)))
