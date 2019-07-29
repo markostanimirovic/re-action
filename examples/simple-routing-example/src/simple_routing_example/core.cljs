4d47416af2cf9b6169631ce9dc827027f5bb6677
stanimirovic <markostanimirovic95@gmail.com>
2019-07-29 22:15:38 +0200

    Created pages and finished initial version of simple routing example.

--- examples/simple-routing-example/src/simple_routing_example/core.cljs
+++ examples/simple-routing-example/src/simple_routing_example/core.cljs
@@ -1,18 +1,50 @@
 (ns simple-routing-example.core
     (:require
-      [reagent.core :as r]))
+      [reagent.core :as r]
+      [re-action.routing :as routing]))
 
-;; -------------------------
-;; Views
+;; === Pages ===
 
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div
+   [:h3 "Home"]
+   [:p "Welcome to home page!"]])
 
-;; -------------------------
-;; Initialize app
+(defn about-page []
+  [:div
+   [:h3 "About"]
+   [:p "This is about page"]])
+
+(defn not-found-page []
+  [:div
+   [:h3 "404 Not Found"]
+   [:p "Oops! Something went wrong"]])
+
+;; === Routes ===
+
+(routing/route "#/home" home-page)
+(routing/route "#/about" about-page)
+(routing/route "#/not-found" not-found-page)
+
+(routing/redirect "" "#/home")
+(routing/redirect "**" "#/not-found")
+
+(routing/start-routing)
+
+;; === App Container ===
+
+(defn app []
+  [:div
+   [:h2 "App Header"]
+   [:button {:on-click #(routing/navigate "")} "Home"]
+   [:button {:on-click #(routing/navigate "#/about")} "About"]
+   [:button {:on-click #(routing/navigate "#/not-defined-route")} "Not Defined Route"]
+   [:br]
+   [@routing/router-outlet]
+   [:small "App Footer"]])
 
 (defn mount-root []
-  (r/render [home-page] (.getElementById js/document "app")))
+  (r/render [app] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
