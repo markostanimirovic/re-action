86a4331eb8797680dea80660937f4c42557481ab
stanimirovic <markostanimirovic95@gmail.com>
2019-08-04 02:36:06 +0200

    Updated simple routing example to be in accordance with new release of Re-Action library.

--- examples/simple-routing-example/src/simple_routing_example/core.cljs
+++ examples/simple-routing-example/src/simple_routing_example/core.cljs
@@ -1,7 +1,6 @@
 (ns simple-routing-example.core
-    (:require
-      [reagent.core :as r]
-      [re-action.router :as router]))
+  (:require [reagent.core :as r]
+            [re-action.router :as router]))
 
 ;; === Pages ===
 
@@ -10,10 +9,15 @@
    [:h3 "Home"]
    [:p "Welcome to home page!"]])
 
-(defn about-page []
+(defn foo-bar-page []
   [:div
-   [:h3 "About"]
-   [:p "This is about page"]])
+   [:h3 "Foo Bar"]
+   [:p "This is foo bar page"]])
+
+(defn todo-details-page [id]
+  [:div
+   [:h3 "TODO Details"]
+   [:p (str "Body of TODO with id: " id)]])
 
 (defn not-found-page []
   [:div
@@ -23,10 +27,12 @@
 ;; === Routes ===
 
 (router/defroute "/home" home-page)
-(router/defroute "/about/me" about-page)
+(router/defroute "/foo/bar" foo-bar-page)
+(router/defroute "/todo/::id" todo-details-page)
 (router/defroute "/not-found" not-found-page)
 
 (router/redirect "/" "/home")
+(router/redirect "/foo-bar" "/foo/bar")
 (router/redirect "**" "/not-found")
 
 (router/start)
@@ -37,10 +43,12 @@
   [:div
    [:h2 "App Header"]
    [:button {:on-click #(router/navigate "/")} "Home"]
-   [:button {:on-click #(router/navigate "/about/me")} "About"]
+   [:button {:on-click #(router/navigate "/foo/bar")} "Foo Bar"]
+   [:button {:on-click #(router/navigate "/todo/1")} "TODO 1"]
+   [:button {:on-click #(router/navigate "/todo/2")} "TODO 2"]
    [:button {:on-click #(router/navigate "/not-defined-route-123")} "Not Defined Route"]
    [:br]
-   [@router/outlet]
+   (router/outlet)
    [:small "App Footer"]])
 
 (defn mount-root []
