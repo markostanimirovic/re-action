3d44bcba44b1ad5eb45399214e867824e87133ee
stanimirovic <markostanimirovic95@gmail.com>
2019-08-05 19:49:00 +0200

    Updated Re-Action dependencies in example projects.

--- examples/blog-example/project.clj
+++ examples/blog-example/project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
                  [reagent "0.8.1"]
-                 [org.clojars.stanimirovic/re-action "0.2.4"]]
+                 [org.clojars.stanimirovic/re-action "0.2.5"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.19"]]
