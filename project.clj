f48375b911643ac277e8dadcfa43b0aa59d9e024
stanimirovic <markostanimirovic95@gmail.com>
2019-08-11 16:27:49 +0200

    Updated Re-Streamer dependency. New release 0.2.6.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.stanimirovic/re-action "0.2.5"
+(defproject org.clojars.stanimirovic/re-action "0.2.6"
   :description "ClojureScript Framework for Building Single Page Applications"
   :url "https://github.com/stanimirovic/re-action"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [org.clojars.stanimirovic/re-streamer "0.3.6"]])
+                 [org.clojars.stanimirovic/re-streamer "0.3.9"]])
