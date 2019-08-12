1b3819e884c3c280c06acd6d3a9f5f39c9f58e84
stanimirovic <markostanimirovic95@gmail.com>
2019-08-12 22:26:49 +0200

    Created validation mechanism. Refactoring needed.

--- src/re_action/form.cljs
+++ src/re_action/form.cljs
@@ -8,20 +8,31 @@
   (get-in form [id :dirty]))
 
 (defn valid? [form id]
-  (get-in form [id :valid]))
+  (every? false? (vals (get-in form [id :errors]))))
 
 (defn value [form]
   (into {} (map (fn [[id props]] [id (:value props)]) form)))
 
 (defn init [form]
-  (let [controls (r/atom (into {} (map (fn [[id validators]] [id {:touched false :dirty false :value nil}]) form)))]
+  (let [controls (r/atom (into {} (map (fn [[id validators]] [id {:touched false
+                                                                  :dirty   false
+                                                                  :value   nil
+                                                                  :errors  (into {} (map #(-> [(key %) false])
+                                                                                         validators))}])
+                                       form)))]
     (r/after-render
-      #(doseq [[id validators] form]
-         (let [element (.getElementById js/document (name id))]
-           (swap! controls assoc-in [id :value] (.. element -value))
-           (set! (.-oninput element) (fn [e]
-                                       (swap! controls update id
-                                              assoc :value (.. e -target -value) :dirty true)))
-           (set! (.-onfocus element) (fn [e]
-                                       (swap! controls assoc-in [id :touched] true))))))
+      (fn []
+        (doseq [[id validators] form]
+          (let [element (.getElementById js/document (name id))]
+            (swap! controls assoc-in [id :value] (.. element -value))
+            (swap! controls assoc-in [id :errors] (into {}
+                                                        (map (fn [[name f]] [name (not (f (.. element -value)))])
+                                                             validators)))
+            (set! (.-oninput element) (fn [e]
+                                        (swap! controls update id
+                                               assoc :value (.. e -target -value) :dirty true
+                                               :errors (into {} (map (fn [[name f]] [name (not (f (.. e -target -value)))])
+                                                                     validators)))))
+            (set! (.-onfocus element) (fn [e]
+                                        (swap! controls assoc-in [id :touched] true)))))))
     controls))
