1f8816935ba68e689ae9feb708c02eef76f84aec
stanimirovic <markostanimirovic95@gmail.com>
2019-08-14 00:44:03 +0200

    Implemented initial version of login form example.

--- examples/login-form/src/login_form/core.cljs
+++ examples/login-form/src/login_form/core.cljs
@@ -1,18 +1,38 @@
 (ns login-form.core
-    (:require
-      [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [re-action.form :as form]
+            [re-action.session :as session]))
 
-;; -------------------------
-;; Views
+(defn credentials-alert []
+  (let [login-form (:state (session/get :login-form))]
+    (fn []
+      (when (not (nil? @login-form))
+        [:div.alert.alert-success
+         "Username: " (:username @login-form)
+         ", Password: " (:password @login-form)]))))
 
-(defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+(defn login-form []
+  (let [login-form (form/create {:username {:required (comp not empty?)}
+                                 :password {:required (comp not empty?)}})]
+    (fn []
+      [:form {:on-submit #(do (.preventDefault %)
+                              (session/put! :login-form (form/value @login-form)))}
+       [:div.form-group
+        [:label {:for :username} "Username"]
+        [:input.form-control {:id :username :type :text}]]
+       [:div.form-group
+        [:label {:for :password} "Password"]
+        [:input.form-control {:id :password :type :password}]]
+       [:button.btn.btn-primary {:disabled (not (form/valid? @login-form))} "Login"]])))
 
-;; -------------------------
-;; Initialize app
+(defn app []
+  [:div.container
+   [:div.row.justify-content-center
+    [:div.col-md-12 [credentials-alert]]
+    [:div.col-md-12 [login-form]]]])
 
 (defn mount-root []
-  (r/render [home-page] (.getElementById js/document "app")))
+  (r/render [app] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
