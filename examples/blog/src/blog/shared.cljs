1813e1083479002bc80573329b2d7dfc189d2ba8
stanimirovic <markostanimirovic95@gmail.com>
2019-08-17 22:51:26 +0200

    Blog example: Refactored shared namespace.

--- examples/blog/src/blog/shared.cljs
+++ examples/blog/src/blog/shared.cljs
@@ -3,35 +3,32 @@
             [re-action.router :as router]))
 
 (defn header []
-  [:nav {:class "navbar navbar-expand navbar-light bg-light"}
-   [:ul {:class "navbar-nav"}
-    [:li {:class "nav-item"}
-     [:a {:class    "nav-link"
-          :on-click #(router/navigate "/")} "Home"]]
-    [:li {:class "nav-item"}
-     [:a {:class    "nav-link"
-          :on-click #(router/navigate "/posts")} "Posts"]]]])
+  [:nav.navbar.navbar-expand.navbar-light.bg-light
+   [:ul.navbar-nav
+    [:li.nav-item
+     [:a.nav-link {:on-click #(router/navigate "/")} "Home"]]
+    [:li.nav-item
+     [:a.nav-link {:on-click #(router/navigate "/posts")} "Posts"]]]])
 
 (defn footer []
-  [:footer {:class "container buffer-bottom text-center"}
+  [:footer.container.buffer-bottom.text-center
    [:hr]
-   [:span {:class "text-muted"}
+   [:span.text-muted
     (gstring/unescapeEntities "&copy;") " 2019 Marko Stanimirović"]])
 
 (defn shell []
-  [:div {:class "wrapper"}
+  [:div.wrapper
    [header]
-   [:div {:class "container body buffer-top"} (router/outlet)]
+   [:div.container.body.buffer-top (router/outlet)]
    [footer]])
 
 (defn home []
-  [:div {:class "text-center"}
+  [:div.text-center
    [:h1 "Welcome to Clojure Blog!"]
    [:img {:src "https://www.stickpng.com/assets/images/5847eb17cef1014c0b5e4849.png"}]])
 
 (defn not-found []
-  [:div {:class "text-danger text-center"}
+  [:div.text-danger.text-center
    [:h1 "Oops!"]
    [:h2 "Something went wrong."]
-   [:h3 "Take me " [:button {:class    "btn btn-outline-danger btn-lg"
-                             :on-click #(router/navigate "/home")} "home"]]])
+   [:h3 "Take me " [:button.btn.btn-outline-danger.btn-lg {:on-click #(router/navigate "/home")} "home"]]])
