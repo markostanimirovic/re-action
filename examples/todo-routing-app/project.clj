db0f1413737423a5269b21374576c20a0b68ea21
stanimirovic <markostanimirovic95@gmail.com>
2019-08-18 02:10:28 +0200

    Todo Routing App: Updated Re-Action dependency.

--- examples/todo-routing-app/project.clj
+++ examples/todo-routing-app/project.clj
@@ -6,7 +6,7 @@
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [org.clojars.stanimirovic/re-action "0.3.0"]]
+                 [org.clojars.stanimirovic/re-action "0.3.1"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.19"]]
